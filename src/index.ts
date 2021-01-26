49c49e55852a5b1fad4d18faf0c3827c585927a8
Josh Kennedy <jkennedy@Lappy-Reloaded-2020-2.local>
2021-01-26 10:34:08 -0800

    add missing decorators

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,22 @@ import 'reflect-metadata'
 import ArrayItems from './decorators/basic/ArrayItems'
 import Description from './decorators/basic/Description'
 import ISO8601Date from './decorators/basic/ISO8601Date'
-import MaxLength from './decorators/basic/MaximumLength'
-import MinLength from './decorators/basic/MinimumLength'
 import Required from './decorators/basic/Required'
 import SubObject from './decorators/basic/SubObject'
 import PropertyCollector from './lib/PropertyCollector'
 import BasicPropertyDescriptor from './lib/BasicPropertyDescriptor'
 import EntityPropertyDescriptors from './lib/EntityPropertyDescriptors'
+import Email from './decorators/basic/Email'
+import Enumeration from './decorators/basic/Enumeration'
+import MaximumLength from './decorators/basic/MaximumLength'
+import MaximumValue from './decorators/basic/MaximumValue'
+import MinimumLength from './decorators/basic/MinimumLength'
+import MinimumValue from './decorators/basic/MinimumValue'
+import NegativeValue from './decorators/basic/NegativeValue'
+import Optional from './decorators/basic/Optional'
+import Phone from './decorators/basic/Phone'
+import PositiveValue from './decorators/basic/PositiveValue'
+import Url from './decorators/basic/Url'
 
 export {
     PropertyCollector,
@@ -17,9 +26,18 @@ export {
     EntityPropertyDescriptors,
     ArrayItems,
     Description,
+    Email,
+    Enumeration,
     ISO8601Date,
-    MaxLength,
-    MinLength,
+    MaximumLength,
+    MaximumValue,
+    MinimumLength,
+    MinimumValue,
+    NegativeValue,
+    Optional,
+    Phone,
+    PositiveValue,
     Required,
-    SubObject
+    SubObject,
+    Url
 }
